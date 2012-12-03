e3836db023d20c1542f7c3532e8a7666f41ee02c
humboldt <humboldt_xie@163.com>
2012-12-03 22:09:12 +0800

    码谱构思

--- index.php
+++ index.php
@@ -9,21 +9,22 @@ require('third/smarty/Smarty.class.php');
 
 $smarty = new Smarty;
 
+//smarty setting
 //$smarty->force_compile = true;
 $smarty->debugging = true;
 $smarty->caching = true;
 $smarty->cache_lifetime = 120;
 $subdir="third". DS."smarty";
-$pojdir="test";
 $smarty->setCompileDir($subdir . DS.'templates_c' . DS)
 	->setCacheDir($subdir . DS.'cache' . DS);
-
-
-$smarty->setTemplateDir( ".". DS . $pojdir . DS.'templates'. DS)
-	->setConfigDir(".". DS . $pojdir . DS.'configs' . DS);
 	
-	
-include_once("test"."/index.php");
+//project setting
+$project=$_GET["project"];
+if($project=="")
+	$project="test";
+$smarty->setTemplateDir( ".". DS . $project . DS.'templates'. DS)
+	->setConfigDir(".". DS . $project . DS.'configs' . DS);
 
+include_once($project."/index.php");
 $smarty->display('index.tpl');
 ?>
