9768f74c634e193d6fc4925064ac2e94892a3319
humboldt <humboldt_xie@163.com>
2012-12-19 20:49:58 +0800

    添加edit

---
+++ codebook/codebook_edit.php
@@ -0,0 +1,21 @@
+﻿<?php
+ /**
+ * Example Application
+
+ * @package Example-application
+ */	
+
+$smarty->caching = false;
+$template="codebook_edit.tpl";
+
+if(!$smarty->isCached($template,"edit".$code_id)) {
+	//$DB=new Mysql("codebook");
+	//$row=$DB->fetch("SELECT * from `t_codebook` where id=:id",array(id=>$code_id));
+	//if($row){
+		//foreach($row as $key => $value){
+		//	$smarty->assign($key,$value);
+		//}
+	//}
+}
+$smarty->display($template,$code_id);
+?>
